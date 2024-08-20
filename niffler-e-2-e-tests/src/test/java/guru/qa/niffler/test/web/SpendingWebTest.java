7b27d632303ad8bef4aa639fe13c848222fb4d0b
dtuchs <dtuchs@gmail.com>
2024-08-20 17:42:46 +0300

    stream #5 - JUnit Extensions: Meta-annotations, global extension registration

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.model.SpendJson;
@@ -10,7 +11,7 @@ import guru.qa.niffler.page.MainPage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(BrowserExtension.class)
+@WebTest
 public class SpendingWebTest {
 
   private static final Config CFG = Config.getInstance();

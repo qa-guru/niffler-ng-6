76dd547810abb8db540a88b3a3db627e59e8e989
dtuchs <dtuchs@gmail.com>
2024-08-20 17:42:46 +0300

    Homeworks for streams #1-#4 - JUnit Extensions

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -2,17 +2,35 @@ package guru.qa.niffler.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
+
   private final SelenideElement usernameInput = $("input[name='username']");
   private final SelenideElement passwordInput = $("input[name='password']");
   private final SelenideElement submitButton = $("button[type='submit']");
+  private final SelenideElement registerButton = $("a[href='/register']");
+  private final SelenideElement errorContainer = $(".form__error");
 
-  public MainPage login(String username, String password) {
+  public RegisterPage doRegister() {
+    registerButton.click();
+    return new RegisterPage();
+  }
+
+  public MainPage successLogin(String username, String password) {
+    login(username, password);
+    return new MainPage();
+  }
+
+  public void login(String username, String password) {
     usernameInput.setValue(username);
     passwordInput.setValue(password);
     submitButton.click();
-    return new MainPage();
+  }
+
+  public LoginPage checkError(String error) {
+    errorContainer.shouldHave(text(error));
+    return this;
   }
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -0,0 +1,30 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.extension.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BrowserExtension.class)
+public class LoginTest {
+
+  private static final Config CFG = Config.getInstance();
+  private static final Faker faker = new Faker();
+
+  @Test
+  void mainPageShouldBeDisplayedAfterSuccessLogin() {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .successLogin("duck", "12345")
+        .checkThatPageLoaded();
+  }
+
+  @Test
+  void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+    LoginPage loginPage = Selenide.open(CFG.frontUrl(), LoginPage.class);
+    loginPage.login(faker.name().username(), "BAD");
+    loginPage.checkError("Bad credentials");
+  }
+}

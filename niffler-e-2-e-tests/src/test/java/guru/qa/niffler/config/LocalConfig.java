6c7927f296e0972d9137ada21c56222e3d207e5f
dtuchs <dtuchs@gmail.com>
2024-08-21 22:20:32 +0300

    stream #1 - intro to QA.GURU Advanced NG

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
@@ -0,0 +1,15 @@
+package guru.qa.niffler.config;
+
+enum LocalConfig implements Config {
+  INSTANCE;
+
+  @Override
+  public String frontUrl() {
+    return "http://127.0.0.1:3000/";
+  }
+
+  @Override
+  public String spendUrl() {
+    return "http://127.0.0.1:8093/";
+  }
+}
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -0,0 +1,33 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.Spending;
+import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.Test;
+
+public class SpendingWebTest {
+
+  private static final Config CFG = Config.getInstance();
+
+  @Spending(
+      username = "duck",
+      category = "Обучение",
+      description = "Обучение Advanced 2.0",
+      amount = 79990
+  )
+  @Test
+  void categoryDescriptionShouldBeChangedFromTable(SpendJson spend) {
+    final String newDescription = "Обучение Niffler Next Generation";
+
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .login("duck", "12345")
+        .editSpending(spend.description())
+        .setNewSpendingDescription(newDescription)
+        .save();
+
+    new MainPage().checkThatTableContainsSpending(newDescription);
+  }
+}

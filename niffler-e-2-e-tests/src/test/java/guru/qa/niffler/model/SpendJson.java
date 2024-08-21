ae6bd187172e397ca85b05ef8c63083a200c82c8
dtuchs <dtuchs@gmail.com>
2024-08-21 22:20:32 +0300

    stream #1 - intro to QA.GURU Advanced NG

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/model/SpendJson.java
@@ -0,0 +1,24 @@
+package guru.qa.niffler.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record SpendJson(
+    @JsonProperty("id")
+    UUID id,
+    @JsonProperty("spendDate")
+    Date spendDate,
+    @JsonProperty("category")
+    CategoryJson category,
+    @JsonProperty("currency")
+    CurrencyValues currency,
+    @JsonProperty("amount")
+    Double amount,
+    @JsonProperty("description")
+    String description,
+    @JsonProperty("username")
+    String username) {
+
+}

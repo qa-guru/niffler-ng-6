cc1c2dabcfc24e44fe44f87c28cf9b4365a740c6
dtuchs <dtuchs@gmail.com>
2024-08-28 12:57:23 +0500

    stream #4 - JUnit Extensions: Users queue, GH Extension

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/GhApi.java
@@ -0,0 +1,19 @@
+package guru.qa.niffler.api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+
+public interface GhApi {
+
+  @GET("repos/qa-guru/niffler/issues/{issue_number}")
+  @Headers({
+      "Accept: application/vnd.github+json",
+      "X-GitHub-Api-Version: 2022-11-28"
+  })
+  Call<JsonNode> issue(@Header("Authorization") String bearerToken,
+                       @Path("issue_number") String issueNumber);
+}

cdbb9fa00797ca2c0601f2d1d27fc46995361d23
dtuchs <dtuchs@gmail.com>
2024-09-03 00:32:54 +0500

    stream #7 - JDBC: transactions

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoJdbc.java
@@ -1,7 +1,6 @@
 package guru.qa.niffler.data.dao.impl;
 
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.data.Databases;
 import guru.qa.niffler.data.dao.CategoryDao;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 
@@ -17,31 +16,35 @@ public class CategoryDaoJdbc implements CategoryDao {
 
   private static final Config CFG = Config.getInstance();
 
+  private final Connection connection;
+
+  public CategoryDaoJdbc(Connection connection) {
+    this.connection = connection;
+  }
+
   @Override
   public CategoryEntity create(CategoryEntity category) {
-    try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
-      try (PreparedStatement ps = connection.prepareStatement(
-          "INSERT INTO category (username, name, archived) " +
-              "VALUES (?, ?, ?)",
-          Statement.RETURN_GENERATED_KEYS
-      )) {
-        ps.setString(1, category.getUsername());
-        ps.setString(2, category.getName());
-        ps.setBoolean(3, category.isArchived());
+    try (PreparedStatement ps = connection.prepareStatement(
+        "INSERT INTO category (username, name, archived) " +
+            "VALUES (?, ?, ?)",
+        Statement.RETURN_GENERATED_KEYS
+    )) {
+      ps.setString(1, category.getUsername());
+      ps.setString(2, category.getName());
+      ps.setBoolean(3, category.isArchived());
 
-        ps.executeUpdate();
+      ps.executeUpdate();
 
-        final UUID generatedKey;
-        try (ResultSet rs = ps.getGeneratedKeys()) {
-          if (rs.next()) {
-            generatedKey = rs.getObject("id", UUID.class);
-          } else {
-            throw new SQLException("Can`t find id in ResultSet");
-          }
+      final UUID generatedKey;
+      try (ResultSet rs = ps.getGeneratedKeys()) {
+        if (rs.next()) {
+          generatedKey = rs.getObject("id", UUID.class);
+        } else {
+          throw new SQLException("Can`t find id in ResultSet");
         }
-        category.setId(generatedKey);
-        return category;
       }
+      category.setId(generatedKey);
+      return category;
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -49,23 +52,21 @@ public class CategoryDaoJdbc implements CategoryDao {
 
   @Override
   public Optional<CategoryEntity> findCategoryById(UUID id) {
-    try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
-      try (PreparedStatement ps = connection.prepareStatement(
-          "SELECT * FROM category WHERE id = ?"
-      )) {
-        ps.setObject(1, id);
-        ps.execute();
-        try (ResultSet rs = ps.getResultSet()) {
-          if (rs.next()) {
-            CategoryEntity ce = new CategoryEntity();
-            ce.setId(rs.getObject("id", UUID.class));
-            ce.setUsername(rs.getString("username"));
-            ce.setName(rs.getString("name"));
-            ce.setArchived(rs.getBoolean("archived"));
-            return Optional.of(ce);
-          } else {
-            return Optional.empty();
-          }
+    try (PreparedStatement ps = connection.prepareStatement(
+        "SELECT * FROM category WHERE id = ?"
+    )) {
+      ps.setObject(1, id);
+      ps.execute();
+      try (ResultSet rs = ps.getResultSet()) {
+        if (rs.next()) {
+          CategoryEntity ce = new CategoryEntity();
+          ce.setId(rs.getObject("id", UUID.class));
+          ce.setUsername(rs.getString("username"));
+          ce.setName(rs.getString("name"));
+          ce.setArchived(rs.getBoolean("archived"));
+          return Optional.of(ce);
+        } else {
+          return Optional.empty();
         }
       }
     } catch (SQLException e) {
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/SpendDaoJdbc.java
@@ -1,7 +1,6 @@
 package guru.qa.niffler.data.dao.impl;
 
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.data.Databases;
 import guru.qa.niffler.data.dao.SpendDao;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
 
@@ -16,34 +15,38 @@ public class SpendDaoJdbc implements SpendDao {
 
   private static final Config CFG = Config.getInstance();
 
+  private final Connection connection;
+
+  public SpendDaoJdbc(Connection connection) {
+    this.connection = connection;
+  }
+
   @Override
   public SpendEntity create(SpendEntity spend) {
-    try (Connection connection = Databases.connection(CFG.spendJdbcUrl())) {
-      try (PreparedStatement ps = connection.prepareStatement(
-          "INSERT INTO spend (username, spend_date, currency, amount, description, category_id) " +
-              "VALUES ( ?, ?, ?, ?, ?, ?)",
-          Statement.RETURN_GENERATED_KEYS
-      )) {
-        ps.setString(1, spend.getUsername());
-        ps.setDate(2, spend.getSpendDate());
-        ps.setString(3, spend.getCurrency().name());
-        ps.setDouble(4, spend.getAmount());
-        ps.setString(5, spend.getDescription());
-        ps.setObject(6, spend.getCategory().getId());
+    try (PreparedStatement ps = connection.prepareStatement(
+        "INSERT INTO spend (username, spend_date, currency, amount, description, category_id) " +
+            "VALUES ( ?, ?, ?, ?, ?, ?)",
+        Statement.RETURN_GENERATED_KEYS
+    )) {
+      ps.setString(1, spend.getUsername());
+      ps.setDate(2, spend.getSpendDate());
+      ps.setString(3, spend.getCurrency().name());
+      ps.setDouble(4, spend.getAmount());
+      ps.setString(5, spend.getDescription());
+      ps.setObject(6, spend.getCategory().getId());
 
-        ps.executeUpdate();
+      ps.executeUpdate();
 
-        final UUID generatedKey;
-        try (ResultSet rs = ps.getGeneratedKeys()) {
-          if (rs.next()) {
-            generatedKey = rs.getObject("id", UUID.class);
-          } else {
-            throw new SQLException("Can`t find id in ResultSet");
-          }
+      final UUID generatedKey;
+      try (ResultSet rs = ps.getGeneratedKeys()) {
+        if (rs.next()) {
+          generatedKey = rs.getObject("id", UUID.class);
+        } else {
+          throw new SQLException("Can`t find id in ResultSet");
         }
-        spend.setId(generatedKey);
-        return spend;
       }
+      spend.setId(generatedKey);
+      return spend;
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }

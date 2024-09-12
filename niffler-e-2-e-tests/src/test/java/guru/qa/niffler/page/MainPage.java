8ea53f125503921dca43231b5da824fd5831b5bd
Orkhan Abbasov <orkhan.abbasov@pashapay.az>
2024-09-12 22:00:51 +0400

    HW_1: add login and register tests

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.page;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -8,6 +9,10 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class MainPage {
   private final ElementsCollection tableRows = $("#spendings tbody").$$("tr");
+  private final SelenideElement menuButton = $("button[aria-label='Menu']");
+  private final SelenideElement profileButton = $("a[href='/profile']");
+  private final SelenideElement spendingHeader = $(".css-uxhuts");
+  private final SelenideElement statisticsHeader = $(".css-giaux5");
 
   public EditSpendingPage editSpending(String spendingDescription) {
     tableRows.find(text(spendingDescription)).$$("td").get(5).click();
@@ -16,5 +21,15 @@ public class MainPage {
 
   public void checkThatTableContainsSpending(String spendingDescription) {
     tableRows.find(text(spendingDescription)).should(visible);
+  }
+
+  public MainPage checkStatisticsHeader(String title) {
+    statisticsHeader.shouldHave(text(title));
+    return this;
+  }
+
+  public MainPage checkSpendingHeader(String title) {
+    spendingHeader.shouldHave(text(title));
+    return this;
   }
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTests.java
@@ -0,0 +1,30 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+
+public class LoginTests {
+
+    private static final Config CFG = Config.getInstance();
+    private static final String STATISTIC_HEADER = "Statistics";
+    private static final String SPENDING_HEADER = "History of Spendings";
+
+    @Test
+    public void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+        final String errorTitle = "Неверные учетные данные пользователя";
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("not_duck", "12345");
+
+        new LoginPage().checkErrorTitle(errorTitle);
+    }
+
+    @Test
+    public void mainPageShouldBeDisplayAfterSuccessLogin() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("duck", "12345")
+                .checkSpendingHeader(SPENDING_HEADER)
+                .checkStatisticsHeader(STATISTIC_HEADER);
+    }
+}

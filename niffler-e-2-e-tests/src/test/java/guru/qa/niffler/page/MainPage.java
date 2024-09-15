5c5cfeadf448a88e5b9689766b328d73e96b2a12
zveda21 <zveda.hyan@gmail.com>
2024-09-15 22:28:29 +0400

    add locators and methods for FriendPage

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,20 +1,35 @@
 package guru.qa.niffler.page;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class MainPage {
-  private final ElementsCollection tableRows = $("#spendings tbody").$$("tr");
 
-  public EditSpendingPage editSpending(String spendingDescription) {
-    tableRows.find(text(spendingDescription)).$$("td").get(5).click();
-    return new EditSpendingPage();
-  }
+    private final SelenideElement personIcon = $("[data-testid='PersonIcon']");
+    private final SelenideElement friendButton = $("li [href='/people/friends']");
+    private final ElementsCollection tableRows = $("#spendings tbody").$$("tr");
+    private final SelenideElement mainPageRootContainer = $("[class^='MuiBox-root']");
 
-  public void checkThatTableContainsSpending(String spendingDescription) {
-    tableRows.find(text(spendingDescription)).should(visible);
-  }
+    public EditSpendingPage editSpending(String spendingDescription) {
+        tableRows.find(text(spendingDescription)).$$("td").get(5).click();
+        return new EditSpendingPage();
+    }
+
+    public void checkThatTableContainsSpending(String spendingDescription) {
+        tableRows.find(text(spendingDescription)).should(visible);
+    }
+
+    public void checkThatPageContainsRootContainer() {
+        mainPageRootContainer.shouldBe(visible);
+    }
+
+    public FriendPage clickOnFriendButtonUnderPersonIcon() {
+        personIcon.click();
+        friendButton.click();
+        return new FriendPage();
+    }
 }

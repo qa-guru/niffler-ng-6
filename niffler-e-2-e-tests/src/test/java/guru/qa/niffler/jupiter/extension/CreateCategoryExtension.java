32b44a5a908b9846a7fa75b4ebfd75a2e3b1def9
Orkhan Abbasov <orkhan.abbasov@pashapay.az>
2024-09-17 01:03:16 +0400

    HW_1: fixes due to comments, resolve conflicts

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CreateCategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CreateCategoryExtension.java
@@ -54,6 +54,7 @@ public class CreateCategoryExtension implements BeforeEachCallback, AfterEachCal
                 category.username(),
                 true
         );
+
         categoryApiClient.updateCategory(archivedCategory);
     }
 }

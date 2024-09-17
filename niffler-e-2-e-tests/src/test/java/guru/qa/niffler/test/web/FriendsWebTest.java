47642fc7f9b04be42b6379f72f8b0afae2b2c28c
Antonov Maksim <maks.antonov.2012@gmail.com>
2024-09-17 12:52:30 +0300

    рефакторинг

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -4,8 +4,8 @@ import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
+import guru.qa.niffler.page.FriendsPage;
 import guru.qa.niffler.page.LoginPage;
-import guru.qa.niffler.page.TopMenu;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -19,9 +19,11 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void friendShouldBePresentInFriendsTable(@UserType(type = WITH_FRIEND) StaticUser user) {
-        Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.password());
-        new TopMenu().goToFriendsPage()
+        FriendsPage friendsPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .goToFriends();
+
+        friendsPage
                 .checkNameInFriendList(user.friend())
                 .myFriendsHeaderShouldBeVisible()
                 .friendRequestsHeaderShouldNotBeVisible();
@@ -30,9 +32,10 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void friendsTableShouldBeEmptyForNewUser(@UserType(type = EMPTY) StaticUser user) {
-        Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.password());
-        new TopMenu().goToFriendsPage()
+        FriendsPage friendsPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password()).goToFriends();
+
+        friendsPage
                 .friendListShouldBeEmpty()
                 .myFriendsHeaderShouldNotBeVisible()
                 .friendRequestsHeaderShouldNotBeVisible();
@@ -41,9 +44,10 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void incomeInvitationBePresentInFriendsTable(@UserType(type = WITH_INCOME_REQUEST) StaticUser user) {
-        Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.password());
-        new TopMenu().goToFriendsPage()
+        FriendsPage friendsPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password()).goToFriends();
+
+        friendsPage
                 .checkNameInRequestList(user.income())
                 .myFriendsHeaderShouldBeVisible()
                 .friendRequestsHeaderShouldBeVisible();
@@ -52,9 +56,11 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void outcomeInvitationBePresentInAllPeoplesTable(@UserType(type = WITH_OUTCOME_REQUEST) StaticUser user) {
-        Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login(user.username(), user.password());
-        new TopMenu().goToFriendsPage()
+        FriendsPage friendsPage = Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .goToFriends();
+
+        friendsPage
                 .clickAllPeople()
                 .checkNameInAllPeopleList(user.outcome())
                 .checkOutcomeInvitationInAllPeopleList(user.outcome());

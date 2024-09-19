6bc3a0fc9f211b4e5487211b75dcb943f361c671
sa.erchenkov <sa.erchenkov@inforser.ru>
2024-09-19 16:19:57 +0300

    homework2 part 5

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extantion/UserQueueExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extantion/UserQueueExtension.java
@@ -17,22 +17,29 @@ public class UserQueueExtension implements BeforeEachCallback, AfterEachCallback
 
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(UserQueueExtension.class);
 
-    public record StaticUser(String username, String password, boolean empty) {
+    public record StaticUser(String username, String password, String friends, String income, String outcome) {
     }
 
-    private static final Queue<StaticUser> EMPTY_USER = new ConcurrentLinkedDeque<>();
-    private static final Queue<StaticUser> NOT_EMPTY_USER = new ConcurrentLinkedDeque<>();
+    private static final Queue<UserQueueExtension.StaticUser> EMPTY_USER = new ConcurrentLinkedDeque<>();
+    private static final Queue<UserQueueExtension.StaticUser> WITH_FRIEND_USER = new ConcurrentLinkedDeque<>();
+    private static final Queue<UserQueueExtension.StaticUser> WITH_INCOME_REQUEST_USER = new ConcurrentLinkedDeque<>();
+    private static final Queue<UserQueueExtension.StaticUser> WITH_OUTCOME_REQUEST_USER = new ConcurrentLinkedDeque<>();
 
     static {
-        EMPTY_USER.add(new StaticUser("user1", "12345", true));
-        NOT_EMPTY_USER.add(new StaticUser("esa", "12345", false));
-        NOT_EMPTY_USER.add(new StaticUser("user2", "12345", false));
+        EMPTY_USER.add(new UserQueueExtension.StaticUser("user1", "12345", null, null, null));
+        WITH_FRIEND_USER.add(new UserQueueExtension.StaticUser("esa", "12345", "duck", null, null));
+        WITH_INCOME_REQUEST_USER.add(new UserQueueExtension.StaticUser("user2", "12345", null, "test3", null));
+        WITH_OUTCOME_REQUEST_USER.add(new UserQueueExtension.StaticUser("test3", "12345", null, null, "user2"));
     }
 
     @Target(ElementType.PARAMETER)
     @Retention(RetentionPolicy.RUNTIME)
     public @interface UserType {
-        boolean empty() default true;
+        UserQueueExtension.UserType.Type value() default UserQueueExtension.UserType.Type.EMPTY;
+
+        enum Type {
+            EMPTY, WITH_FRIEND, WITH_INCOME_REQUEST, WITH_OUTCOME_REQUEST
+        }
     }
 
     @Override
@@ -45,9 +52,15 @@ public class UserQueueExtension implements BeforeEachCallback, AfterEachCallback
                             Optional<StaticUser> user = Optional.empty();
                             StopWatch sw = StopWatch.createStarted();
                             while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
-                                user = ut.empty()
-                                        ? Optional.ofNullable(EMPTY_USER.poll())
-                                        : Optional.ofNullable(NOT_EMPTY_USER.poll());
+                                if (ut.value() == UserQueueExtension.UserType.Type.EMPTY) {
+                                    user = Optional.ofNullable(EMPTY_USER.poll());
+                                } else if (ut.value() == UserQueueExtension.UserType.Type.WITH_FRIEND) {
+                                    user = Optional.ofNullable(WITH_FRIEND_USER.poll());
+                                } else if (ut.value() == UserQueueExtension.UserType.Type.WITH_INCOME_REQUEST) {
+                                    user = Optional.ofNullable(WITH_INCOME_REQUEST_USER.poll());
+                                } else if (ut.value() == UserQueueExtension.UserType.Type.WITH_OUTCOME_REQUEST) {
+                                    user = Optional.ofNullable(WITH_OUTCOME_REQUEST_USER.poll());
+                                }
                             }
                             Allure.getLifecycle().updateTestCase(testCase -> {
                                 testCase.setStart(new Date().getTime());
@@ -72,11 +85,14 @@ public class UserQueueExtension implements BeforeEachCallback, AfterEachCallback
     public void afterEach(ExtensionContext context) throws Exception {
         Map<UserType, StaticUser> map = context.getStore(NAMESPACE).get(context.getUniqueId(), Map.class);
         for (Map.Entry<UserType, StaticUser> e : map.entrySet()) {
-            System.out.println("return queue " + e.getValue());
-            if (e.getKey().empty()) {
+            if (e.getValue().friends == null && e.getValue().income == null && e.getValue().outcome == null) {
                 EMPTY_USER.add(e.getValue());
-            } else {
-                NOT_EMPTY_USER.add(e.getValue());
+            } else if (e.getValue().friends != null && e.getValue().income == null && e.getValue().outcome == null) {
+                WITH_FRIEND_USER.add(e.getValue());
+            } else if (e.getValue().friends == null && e.getValue().income != null && e.getValue().outcome == null) {
+                WITH_INCOME_REQUEST_USER.add(e.getValue());
+            } else if (e.getValue().friends == null && e.getValue().income == null && e.getValue().outcome != null) {
+                WITH_OUTCOME_REQUEST_USER.add(e.getValue());
             }
         }
     }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -2,8 +2,8 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.extantion.UserFriensQueueExtension;
-import guru.qa.niffler.jupiter.extantion.UserFriensQueueExtension.StaticUser;
+import guru.qa.niffler.jupiter.extantion.UserQueueExtension;
+import guru.qa.niffler.jupiter.extantion.UserQueueExtension.StaticUser;
 import guru.qa.niffler.page.FriendsPage;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import guru.qa.niffler.jupiter.extantion.BrowserExtension;
 
 
-import static guru.qa.niffler.jupiter.extantion.UserFriensQueueExtension.UserType.Type.*;
+import static guru.qa.niffler.jupiter.extantion.UserQueueExtension.UserType.Type.*;
 
 @ExtendWith(BrowserExtension.class)
 public class FriendsWebTest {
@@ -19,8 +19,8 @@ public class FriendsWebTest {
     private static final Config CFG = Config.getInstance();
 
     @Test
-    @ExtendWith(UserFriensQueueExtension.class)
-    void friendShouldBePresentInFriendsTable(@UserFriensQueueExtension.UserType(WITH_FRIEND) StaticUser user) {
+    @ExtendWith(UserQueueExtension.class)
+    void friendShouldBePresentInFriendsTable(@UserQueueExtension.UserType(WITH_FRIEND) StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
                 .login(user.username(), user.password())
                 .openFriendsPage()
@@ -28,8 +28,8 @@ public class FriendsWebTest {
     }
 
     @Test
-    @ExtendWith(UserFriensQueueExtension.class)
-    void friendsTableShouldBeEmptyForNewUser(@UserFriensQueueExtension.UserType(EMPTY) StaticUser user) {
+    @ExtendWith(UserQueueExtension.class)
+    void friendsTableShouldBeEmptyForNewUser(@UserQueueExtension.UserType(EMPTY) StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
                 .login(user.username(), user.password())
                 .openFriendsPage()
@@ -38,8 +38,8 @@ public class FriendsWebTest {
     }
 
     @Test
-    @ExtendWith(UserFriensQueueExtension.class)
-    void incomeInvitationBePresentInFriendsTable(@UserFriensQueueExtension.UserType(WITH_INCOME_REQUEST) StaticUser user) {
+    @ExtendWith(UserQueueExtension.class)
+    void incomeInvitationBePresentInFriendsTable(@UserQueueExtension.UserType(WITH_INCOME_REQUEST) StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
                 .login(user.username(), user.password())
                 .openFriendsPage()
@@ -47,8 +47,8 @@ public class FriendsWebTest {
     }
 
     @Test
-    @ExtendWith(UserFriensQueueExtension.class)
-    void outcomeInvitationBePresentInAllPeoplesTable(@UserFriensQueueExtension.UserType(WITH_OUTCOME_REQUEST) StaticUser user) {
+    @ExtendWith(UserQueueExtension.class)
+    void outcomeInvitationBePresentInAllPeoplesTable(@UserQueueExtension.UserType(WITH_OUTCOME_REQUEST) StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
                 .login(user.username(), user.password())
                 .openAllPeoplePage()

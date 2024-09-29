0885d1b44ec11aff5c0e09a61bd1fcfb14158fd5
Orkhan Abbasov <orkhan.abbasov@pashapay.az>
2024-09-29 22:43:19 +0400

    HW_4:  resolve conflicts

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.service;
 
-import guru.qa.niffler.config.Config;
+import guru.qa.niffler.data.dao.CategoryDao;
+import guru.qa.niffler.data.dao.SpendDao;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;

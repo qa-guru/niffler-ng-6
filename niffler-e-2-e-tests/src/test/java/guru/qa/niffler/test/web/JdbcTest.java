8bb7bf2db977c0156166343d5feb18ba36c5730f
a.zavrichko <andrey@zavrichko.dev>
2024-10-01 19:47:59 +0300

    HW-7

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
@@ -1,15 +1,87 @@
 package guru.qa.niffler.test.web;
 
+import guru.qa.niffler.model.CurrencyValues;
+import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.service.UsersDbClient;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
+import static guru.qa.niffler.utils.RandomDataUtils.randomUsername;
+
 public class JdbcTest {
+
     @Test
     void springJdbcTest() {
         UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUser(
+                new UserJson(
+                        null,
+                        "user-spring",
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        null
+                )
+        );
+        System.out.println(user);
+    }
 
+    @Test
+    void springJdbcWithoutTransactionTest() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithoutSpringJdbcTransaction(
+                new UserJson(
+                        null,
+                        randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        null
+                )
+        );
+        System.out.println(user);
+    }
 
+    @Test
+    void jdbcTest() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserJdbcTransaction(
+                new UserJson(
+                        null,
+                        "user-jdbc",
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        null
+                )
+        );
+        System.out.println(user);
+    }
+
+    @Test
+    void jdbcWithoutTransactionTest() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithoutJdbcTransaction(
+                new UserJson(
+                        null,
+                        randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        null
+                )
+        );
+        System.out.println(user);
     }
 }

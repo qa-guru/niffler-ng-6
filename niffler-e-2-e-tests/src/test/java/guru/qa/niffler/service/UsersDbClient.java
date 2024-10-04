b16f73ae3d581e1f45d63b223d80c38436be02e7
Serghei Garaga <garaga07@yandex.ru>
2024-10-05 01:21:35 +0300

    выполнение дз №8

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UsersDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UsersDbClient.java
@@ -1,32 +1,33 @@
 package guru.qa.niffler.service;
 
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.data.dao.UdUserDao;
-import guru.qa.niffler.data.dao.impl.UdUserDaoSpringJdbc;
 import guru.qa.niffler.data.entity.auth.AuthUserEntity;
 import guru.qa.niffler.data.entity.auth.Authority;
 import guru.qa.niffler.data.entity.auth.AuthorityEntity;
+import guru.qa.niffler.data.entity.userdata.FriendshipStatus;
 import guru.qa.niffler.data.entity.userdata.UserEntity;
 import guru.qa.niffler.data.repository.AuthUserRepository;
+import guru.qa.niffler.data.repository.UdUserRepository;
 import guru.qa.niffler.data.repository.impl.AuthUserRepositoryJdbc;
+import guru.qa.niffler.data.repository.impl.UdUserRepositoryJdbc;
 import guru.qa.niffler.data.tpl.DataSources;
 import guru.qa.niffler.data.tpl.XaTransactionTemplate;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.UserJson;
+import guru.qa.niffler.utils.RandomDataUtils;
+import jaxb.userdata.FriendState;
 import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.support.TransactionTemplate;
-
 import java.util.Arrays;
-
 
 public class UsersDbClient {
 
     private static final Config CFG = Config.getInstance();
     private static final PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
     private final AuthUserRepository authUserRepository = new AuthUserRepositoryJdbc();
-    private final UdUserDao udUserDao = new UdUserDaoSpringJdbc();
+    private final UdUserRepository udUserRepository = new UdUserRepositoryJdbc();
 
     private final TransactionTemplate txTemplate = new TransactionTemplate(
             new JdbcTransactionManager(
@@ -39,31 +40,95 @@ public class UsersDbClient {
             CFG.userdataJdbcUrl()
     );
 
-    public UserJson createUser(UserJson user) {
+    public UserJson createUserWithFriend(UserJson user) {
         return xaTransactionTemplate.execute(() -> {
-                    AuthUserEntity authUser = new AuthUserEntity();
-                    authUser.setUsername(user.username());
-                    authUser.setPassword(pe.encode("12345"));
-                    authUser.setEnabled(true);
-                    authUser.setAccountNonExpired(true);
-                    authUser.setAccountNonLocked(true);
-                    authUser.setCredentialsNonExpired(true);
-                    authUser.setAuthorities(
-                            Arrays.stream(Authority.values()).map(
-                                    e -> {
-                                        AuthorityEntity ae = new AuthorityEntity();
-                                        ae.setUser(authUser);
-                                        ae.setAuthority(e);
-                                        return ae;
-                                    }
-                            ).toList()
-                    );
-                    authUserRepository.create(authUser);
-                    return UserJson.fromEntity(
-                            udUserDao.create(UserEntity.fromJson(user)),
-                            null
-                    );
+            // Сначала создаем основного пользователя в базе niffler-auth
+            AuthUserEntity authUser = new AuthUserEntity();
+            authUser.setUsername(user.username());
+            authUser.setPassword(pe.encode("12345")); // Задаем пароль
+            authUser.setEnabled(true);
+            authUser.setAccountNonExpired(true);
+            authUser.setAccountNonLocked(true);
+            authUser.setCredentialsNonExpired(true);
+            authUser.setAuthorities(
+                    Arrays.stream(Authority.values()).map(e -> {
+                        AuthorityEntity ae = new AuthorityEntity();
+                        ae.setUser(authUser);
+                        ae.setAuthority(e);
+                        return ae;
+                    }).toList()
+            );
+            authUserRepository.create(authUser); // Сохраняем пользователя в niffler-auth
+
+            // Преобразуем основной объект UserJson в сущность для работы с базой niffler-userdata
+            UserEntity udUser = UserEntity.fromJson(user);
+
+            // Сохраняем основного пользователя в базе niffler-userdata, чтобы получить его id
+            udUserRepository.create(udUser);
+
+            UserEntity udFriendUser = null;
+
+            // Проверяем наличие статуса дружбы и необходимость создания второго пользователя (друга)
+            if (user.friendState() != null && user.friendState() != FriendState.VOID) {
+                // Генерируем случайное имя для второго пользователя (друга)
+                String randomUsername = RandomDataUtils.randomUsername();
+                AuthUserEntity friendAuthUser = new AuthUserEntity();
+                friendAuthUser.setUsername(randomUsername);
+                friendAuthUser.setPassword(pe.encode("12345")); // Задаем пароль для друга
+                friendAuthUser.setEnabled(true);
+                friendAuthUser.setAccountNonExpired(true);
+                friendAuthUser.setAccountNonLocked(true);
+                friendAuthUser.setCredentialsNonExpired(true);
+                friendAuthUser.setAuthorities(
+                        Arrays.stream(Authority.values()).map(e -> {
+                            AuthorityEntity ae = new AuthorityEntity();
+                            ae.setUser(friendAuthUser);
+                            ae.setAuthority(e);
+                            return ae;
+                        }).toList()
+                );
+                // Сохраняем друга в niffler-auth
+                authUserRepository.create(friendAuthUser);
+
+                // Преобразуем друга в сущность для работы с базой niffler-userdata
+                UserJson friendUserJson = new UserJson(
+                        null,
+                        randomUsername,
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        null
+                );
+
+                udFriendUser = UserEntity.fromJson(friendUserJson);
+                udUserRepository.create(udFriendUser); // Сохраняем друга в niffler-userdata
+
+                // Добавляем информацию о статусе дружбы между основным пользователем и другом
+                switch (user.friendState()) {
+                    case INVITE_RECEIVED -> {
+                        udUser.addInvitations(udFriendUser); // Основной пользователь получил приглашение
+                        udFriendUser.addFriends(FriendshipStatus.PENDING, udUser); // Друг отправил приглашение
+                    }
+                    case INVITE_SENT -> {
+                        udUser.addFriends(FriendshipStatus.PENDING, udFriendUser); // Основной пользователь отправил приглашение
+                        udFriendUser.addInvitations(udUser); // Друг получил приглашение
+                    }
+                    case FRIEND -> {
+                        udUser.addFriends(FriendshipStatus.ACCEPTED, udFriendUser); // Дружба подтверждена
+                        udFriendUser.addFriends(FriendshipStatus.ACCEPTED, udUser); // Дружба подтверждена
+                    }
                 }
-        );
+
+                // Сохраняем информацию о дружбе для обоих пользователей в базе niffler-userdata
+                udUserRepository.addFriendships(udUser);
+                udUserRepository.addFriendships(udFriendUser);
+            }
+
+            // Возвращаем информацию о созданном пользователе и статусе дружбы (если есть)
+            return UserJson.fromEntity(udUser, user.friendState());
+        });
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/JdbcTest.java
@@ -6,6 +6,8 @@ import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.service.SpendDbClient;
 import guru.qa.niffler.service.UsersDbClient;
+import guru.qa.niffler.utils.RandomDataUtils;
+import jaxb.userdata.FriendState;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -36,13 +38,14 @@ public class JdbcTest {
         System.out.println(spend);
     }
 
+    // Тест на создание пользователя без дружбы (friendState null)
     @Test
-    void springJdbcTest() {
+    void createUserWithoutFriend() {
         UsersDbClient usersDbClient = new UsersDbClient();
-        UserJson user = usersDbClient.createUser(
+        UserJson user = usersDbClient.createUserWithFriend(
                 new UserJson(
                         null,
-                        "valentin-6",
+                        RandomDataUtils.randomUsername(),
                         null,
                         null,
                         null,
@@ -52,6 +55,111 @@ public class JdbcTest {
                         null
                 )
         );
-        System.out.println(user);
+        System.out.println("Created user without friend: " + user);
+    }
+
+    // Тест на создание пользователя с VOID состоянием дружбы
+    @Test
+    void createUserWithVoidFriendState() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithFriend(
+                new UserJson(
+                        null,
+                        RandomDataUtils.randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        FriendState.VOID
+                )
+        );
+
+        // Печатаем данные пользователя в консоль для проверки
+        System.out.println("Created user with VOID friend state: " + user);
+    }
+
+    // Тест на создание пользователя с отправленным приглашением (friendState INVITE_SENT)
+    @Test
+    void createUserWithInviteSent() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithFriend(
+                new UserJson(
+                        null,
+                        RandomDataUtils.randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        FriendState.INVITE_SENT
+                )
+        );
+        System.out.println("Created user with invite sent: " + user);
+    }
+
+    // Тест на создание пользователя с полученным приглашением (friendState INVITE_RECEIVED)
+    @Test
+    void createUserWithInviteReceived() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithFriend(
+                new UserJson(
+                        null,
+                        RandomDataUtils.randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        FriendState.INVITE_RECEIVED
+                )
+        );
+        System.out.println("Created user with invite received: " + user);
+    }
+
+    // Тест на создание пользователя с подтвержденной дружбой (friendState FRIEND)
+    @Test
+    void createUserWithFriendConfirmed() {
+        UsersDbClient usersDbClient = new UsersDbClient();
+        UserJson user = usersDbClient.createUserWithFriend(
+                new UserJson(
+                        null,
+                        RandomDataUtils.randomUsername(),
+                        null,
+                        null,
+                        null,
+                        CurrencyValues.RUB,
+                        null,
+                        null,
+                        FriendState.FRIEND
+                )
+        );
+
+        // Печатаем данные пользователя в консоль для проверки
+        System.out.println("Created user with confirmed friend: " + user);
+    }
+
+    // Имитируем ошибку при создании пользователя, передавая неверные данные
+    @Test
+    void createUserTransactionRollbackOnError() {
+            UsersDbClient usersDbClient = new UsersDbClient();
+            UserJson user = usersDbClient.createUserWithFriend(
+                    new UserJson(
+                            null,
+                            null, // Отсутствует имя пользователя, что вызовет ошибку
+                            null,
+                            null,
+                            null,
+                            CurrencyValues.RUB,
+                            null,
+                            null,
+                            FriendState.FRIEND
+                    )
+            );
+        // Печатаем данные пользователя в консоль для проверки
+        System.out.println("Created user with invalid username: " + user);
     }
 }

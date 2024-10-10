e047e4f475f730ee0faeb3517ad74f6bdad74f11
Arrnel <arrnel@vk.com>
2024-10-10 03:36:26 +0300

    Code from class test module before HW-3_3

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
@@ -1,6 +1,6 @@
 package guru.qa.niffler.jupiter.annotation;
 
-import guru.qa.niffler.jupiter.extension.IssueExtension;
+import guru.qa.niffler.jupiter.extension.CategoryExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.ElementType;
@@ -8,9 +8,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@ExtendWith(IssueExtension.class)
-public @interface DisabledByIssue {
-  String value();
+@Target(ElementType.METHOD)
+@ExtendWith(CategoryExtension.class)
+public @interface Category {
+  String username();
+
+  boolean archived() default false;
+
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -0,0 +1,44 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.ProfilePage;
+import org.junit.jupiter.api.Test;
+
+@WebTest
+public class ProfileTest {
+
+  private static final Config CFG = Config.getInstance();
+
+  @Category(
+      username = "duck",
+      archived = true
+  )
+  @Test
+  void archivedCategoryShouldPresentInCategoriesList(CategoryJson category) {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .successLogin("duck", "12345")
+        .checkThatPageLoaded();
+
+    Selenide.open(CFG.frontUrl() + "profile", ProfilePage.class)
+        .checkArchivedCategoryExists(category.name());
+  }
+
+  @Category(
+      username = "duck",
+      archived = false
+  )
+  @Test
+  void activeCategoryShouldPresentInCategoriesList(CategoryJson category) {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .successLogin("duck", "12345")
+        .checkThatPageLoaded();
+
+    Selenide.open(CFG.frontUrl() + "profile", ProfilePage.class)
+        .checkCategoryExists(category.name());
+  }
+}

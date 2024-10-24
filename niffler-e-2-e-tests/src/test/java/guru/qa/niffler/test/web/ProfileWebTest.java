37907527fcbe0167033b7d29ed2e708706d9ce82
IvanN <github@marscartel.com>
2024-10-23 22:05:51 -0700

    Conflicts resolve

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileWebTest.java
@@ -2,7 +2,7 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.Category;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.page.SignInPage;
 import org.junit.jupiter.api.Test;

983233b1d5b22202307ed315c6d385a80edc16b0
IvanN <github@marscartel.com>
2024-10-23 22:05:51 -0700

    In-class code. Lesson 3.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -16,77 +16,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SpendApiClient {
 
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
-  private final Retrofit retrofit = new Retrofit.Builder()
-      .baseUrl(Config.getInstance().spendUrl())
-      .addConverterFactory(JacksonConverterFactory.create())
-      .build();
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(Config.getInstance().spendUrl())
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
 
-  private final SpendApi spendApi = retrofit.create(SpendApi.class);
+    private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  public SpendJson createSpend(SpendJson spend) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.addSpend(spend)
-              .execute();
-    } catch (IOException e) {
-      throw new AssertionError(e);
+    public SpendJson createSpend(SpendJson spend) {
+        final Response<SpendJson> response;
+        try {
+            response = spendApi.addSpend(spend)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(201, response.code());
+        return response.body();
     }
-    assertEquals(201, response.code());
-    return response.body();
-  }
 
-  public SpendJson editSpend(SpendJson spend) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.editSpend(spend)
-              .execute();
-    } catch (IOException e) {
-      throw new AssertionError(e);
+    public SpendJson editSpend(SpendJson spend) {
+        final Response<SpendJson> response;
+        try {
+            response = spendApi.editSpend(spend)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
     }
-    assertEquals(200, response.code());
-    return response.body();
-  }
 
-  public SpendJson getSpend(String spendId, String username) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.getSpend(spendId, username)
-              .execute();
-    } catch (IOException e) {
-      throw new AssertionError(e);
+    public SpendJson getSpend(String spendId, String username) {
+        final Response<SpendJson> response;
+        try {
+            response = spendApi.getSpend(spendId, username)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
     }
-    assertEquals(200, response.code());
-    return response.body();
-  }
 
-  public List<SpendJson> getSpends(String username, CurrencyValues filterCurrency, Date from, Date to) {
-    // Convert Date to String in the required format, or pass null if not provided
-    String fromDateString = (from != null) ? DATE_FORMAT.format(from) : null;
-    String toDateString = (to != null) ? DATE_FORMAT.format(to) : null;
+    public List<SpendJson> getSpends(String username, CurrencyValues filterCurrency, Date from, Date to) {
+        // Convert Date to String in the required format, or pass null if not provided
+        String fromDateString = (from != null) ? DATE_FORMAT.format(from) : null;
+        String toDateString = (to != null) ? DATE_FORMAT.format(to) : null;
 
-    final Response<List<SpendJson>> response;
-    try {
-      response = spendApi.getSpends(username, filterCurrency, fromDateString, toDateString)
-              .execute();
-    } catch (IOException e) {
-      throw new AssertionError(e);
+        final Response<List<SpendJson>> response;
+        try {
+            response = spendApi.getSpends(username, filterCurrency, fromDateString, toDateString)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
     }
-    assertEquals(200, response.code());
-    return response.body();
-  }
 
-  public SpendJson deleteSpends(String username, List<String> spendIds) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.deleteSpends(username, spendIds)
-              .execute();
-    } catch (IOException e) {
-      throw new AssertionError(e);
+    public SpendJson deleteSpends(String username, List<String> spendIds) {
+        final Response<SpendJson> response;
+        try {
+            response = spendApi.deleteSpends(username, spendIds)
+                    .execute();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        assertEquals(200, response.code());
+        return response.body();
     }
-    assertEquals(200, response.code());
-    return response.body();
-  }
 
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -2,14 +2,28 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.jupiter.Category;
+import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.page.SignInPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.*;
 
 public class ProfileTest {
 
     private static final Config CFG = Config.getInstance();
+
+    // !!!!!!!!!!!!!!!!!!!!!!!! ? delete
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void testWithEmptyUser0(@UserType(empty = true) StaticUser user0,
+                            @UserType(empty = false) StaticUser user1) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println(user0);
+        System.out.println(user1);
+    }
 
     @Category(
             username = "duck",

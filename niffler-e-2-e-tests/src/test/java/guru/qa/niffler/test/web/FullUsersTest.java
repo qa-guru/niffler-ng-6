cf8a1f5b715f78d2ae58fbcbc6a39ca172ad3085
a.zavrichko <andrey@zavrichko.dev>
2024-10-31 16:45:23 +0200

    исправление hw-13

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FullUsersTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FullUsersTest.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.test.web;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Isolated;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@Isolated
+@Order(Integer.MAX_VALUE)
 public class FullUsersTest {
     @User
     @Test

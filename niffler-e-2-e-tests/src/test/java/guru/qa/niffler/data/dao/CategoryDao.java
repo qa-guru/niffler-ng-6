ee57b201977fe5b324603e439fcd32e7fe13c6e1
IvanN <github@marscartel.com>
2024-10-31 21:27:16 -0700

    Homework 4.1. Added Find/Delete DAO

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/CategoryDao.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/CategoryDao.java
@@ -2,10 +2,14 @@ package guru.qa.niffler.data.dao;
 
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryDao {
     CategoryEntity create(CategoryEntity create);
     Optional<CategoryEntity> findCategoryById(UUID id);
+    Optional<CategoryEntity> findCategoryByUsernameAndCategoryName(String username, String categoryName);
+    List<CategoryEntity> findAllByUsername(String username);
+    void deleteCategory(CategoryEntity category);
 }

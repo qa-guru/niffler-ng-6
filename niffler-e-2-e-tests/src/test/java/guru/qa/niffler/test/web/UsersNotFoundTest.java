8e813b74406aa4f4a21fc5521fd51c1d77ebf863
Orkhan Abbasov <orkhan.abbasov@pashapay.az>
2024-11-02 19:20:29 +0400

    HW_13: add Order class annotation

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/UsersNotFoundTest.java
@@ -0,0 +1,21 @@
+package guru.qa.niffler.test.web;
+
+import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+
+@Order(1)
+public class UsersNotFoundTest {
+    private final UsersApiClient apiClient = new UsersApiClient();
+
+    @User()
+    @Test
+    void notFindUser(UserJson user) {
+        assertThat(apiClient.usersAll(user), empty());
+    }
+}

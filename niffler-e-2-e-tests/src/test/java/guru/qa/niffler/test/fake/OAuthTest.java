267b15c9b8c5dc50500942923f9e3fb4094a1cc3
dtuchs <dtuchs@gmail.com>
2025-01-25 18:48:49 +0300

    19.1 - docker infra

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/fake/OAuthTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/fake/OAuthTest.java
@@ -3,11 +3,13 @@ package guru.qa.niffler.test.fake;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.ApiLogin;
 import guru.qa.niffler.jupiter.annotation.Token;
-import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.jupiter.extension.ApiLoginExtension;
 import guru.qa.niffler.service.impl.AuthApiClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ApiLoginExtension.class)
 public class OAuthTest {
 
   private static final Config CFG = Config.getInstance();
@@ -15,8 +17,7 @@ public class OAuthTest {
 
   @Test
   @ApiLogin(username = "duck", password = "12345")
-  void oauthTest(@Token String token, UserJson user) {
-    System.out.println(user);
+  void oauthTest(@Token String token) {
     Assertions.assertNotNull(token);
   }
 }

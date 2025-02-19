84b576967fe7340cf7c52233e3a969d554b7b025
Dmitrii Tuchs <dtuchs@gmail.com>
2025-02-19 15:59:46 +0700

    20.2 - wiremock standalone (#76)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserExtension.java
@@ -3,7 +3,7 @@ package guru.qa.niffler.jupiter.extension;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.service.UsersClient;
-import guru.qa.niffler.service.impl.UsersDbClient;
+import guru.qa.niffler.service.impl.UsersApiClient;
 import guru.qa.niffler.utils.RandomDataUtils;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -17,7 +17,7 @@ public class UserExtension implements BeforeEachCallback, ParameterResolver {
   public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(UserExtension.class);
   private static final String defaultPassword = "12345";
 
-  private final UsersClient usersClient = new UsersDbClient();
+  private final UsersClient usersClient = new UsersApiClient();
 
   @Override
   public void beforeEach(ExtensionContext context) throws Exception {
---
+++ niffler-gateway/src/test/java/guru/qa/niffler/controller/CurrencyControllerTest.java
@@ -0,0 +1,40 @@
+package guru.qa.niffler.controller;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Disabled
+class CurrencyControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void categoriesListShouldBeReturnedForCurrentUser() throws Exception {
+    final String fixtureUser = "bee";
+
+    mockMvc.perform(get("/api/currencies/all")
+            .with(jwt().jwt(c -> c.claim("sub", fixtureUser))))
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
+}
